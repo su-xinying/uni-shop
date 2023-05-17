8193a86159089f635ec07f6d8188b4b29c099f5e
suuxy <578981951@qq.com>
2023-05-17 22:47:44 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+
+// 请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest=function(options){
+	// 展示loading效果
+	uni.showLoading({
+		title:"数据加载中..."
+	})
+	
+}
+
+//响应拦截器
+$http.afterRequest = function () {
+  //关闭loading效果
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg =function(title='数据加载失败!',duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
